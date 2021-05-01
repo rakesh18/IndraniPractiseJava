bea3500d8105eebdbc83efb1959613d1db3247f9
Indrani Jana <in.jana09@gmail.com>
2021-05-01 18:24:14 +0530

    Subject: [PATCH] client-io: adding helloworld feature
        Source: XYZ.com
        MR: 0001
        Type: Integration
        Disposition: Local
        ChangeID: 79a02ac254eece125b83b35d6412f0883caa5571
        Description:
    
        Modifying Hello Azahar feature
    
        The allocated memory is then map to client-IO package  and can be
        verified by running program output-terminal
    
        Signed-off-by: Indrani Jana <in.jana09@gmail.com>

--- HelloWorld1.java
+++ HelloWorld1.java
@@ -11,7 +11,7 @@ public class HelloWorld1 {
 
     public static void main(String[] args) {
        
-        System.out.println("Hello, World");
+        System.out.println("Hello, Azahar");
     }
 
 }
